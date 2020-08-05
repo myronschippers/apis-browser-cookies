b577f7d893a056f0d81d422f6b817a1315e4ebd5
Myron <dubtwee@gmail.com>
2020-08-05 10:23:17 -0500

    removing code that is not being used

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -27,21 +27,9 @@ class App extends Component {
 
   // React renders the content to the application view
   render() {
-    // let myCreature = <h3>You have not chosen a Favorite Creature</h3>;
-
-    // if (this.state.favoriteCreature != null
-    //     && this.state.favoriteCreature.length > 0
-    // ) {
-    //     myCreature = <h3>Your Favorite Creature: {this.state.favoriteCreature}</h3>;
-    // }
-
     return (
       <div>
         <FavoriteCreature creature={this.state.favoriteCreature} />
-        {/* <div className="headerBar">
-          <h1>Browser Cookies</h1>
-          {myCreature}
-        </div> */}
 
         <div className="container">
           <label className="formField">
